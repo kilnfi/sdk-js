2d5baaeb3bbaf6e754e7fe548419919335b17e59
loicttn - pwnh4 <loic.titren@hotmail.fr>
2021-11-16 10:26:36 +0000

    fix(linting): match quotes standard

--- src/skillz.ts
+++ src/skillz.ts
@@ -1,18 +1,18 @@
-import api from "./api";
-import { ValidatorsService } from "./services/validators";
+import api from './api';
+import { ValidatorsService } from './services/validators';
 
-type Network = "prater" | "mainnet";
+type Network = 'prater' | 'mainnet';
 
 export class SkillZ {
   validators: ValidatorsService;
 
   constructor(network: Network, apiToken: string) {
     api.defaults.headers.common.Authorization = `Bearer ${apiToken}`;
-    api.defaults.headers.common["Content-Type"] = `application/json`;
+    api.defaults.headers.common['Content-Type'] = 'application/json';
     api.defaults.baseURL =
-      network === "prater"
-        ? "https://prater.api.skillz.io/"
-        : "https://api.skillz.io/";
+      network === 'prater'
+        ? 'https://prater.api.skillz.io/'
+        : 'https://api.skillz.io/';
 
     this.validators = new ValidatorsService();
   }
