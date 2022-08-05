314fa98a4d7085e5fbb5aa825fe485da1f61f333
Arno Simon <arnos29@gmail.com>
2022-08-05 16:47:58 +0200

    1.0.8

--- src/services/eth.ts
+++ src/services/eth.ts
@@ -30,56 +30,60 @@ export class EthService {
   async craftStakeTx(
     accountId: string,
     walletAddress: string,
-    amount: number
-  ): Promise<EthereumStakeTx | undefined> {
+    amount: number,
+  ): Promise<EthereumStakeTx> {
     if (amount % 32 !== 0 || amount <= 0) {
       throw new InvalidStakeAmount(
-        'Ethereum stake must be a multiple of 32 ETH'
+        'Ethereum stake must be a multiple of 32 ETH',
       );
     }
 
-    // create validation keys via api
-    const { data } = await api.post<InternalBatchDeposit>(
-      '/v1/eth/keys?format=batch_deposit',
-      {
-        withdrawalAddress: walletAddress,
-      },
-      {
-        headers: {
-          "X-Kiln-Account": accountId,
-        }
-      }
-    );
+    try {
+      // create validation keys via api
+      const { data } = await api.post<InternalBatchDeposit>(
+        '/v1/eth/keys?format=batch_deposit',
+        {
+          withdrawalAddress: walletAddress,
+        },
+        {
+          headers: {
+            "X-Kiln-Account": accountId,
+          },
+        },
+      );
 
-    // setup tx variables
-    const batchDeposit = new this.web3.eth.Contract(
-      JSON.parse(BATCH_DEPOSIT_CONTRACT_ABI),
-      BATCH_DEPOSIT_CONTRACT_ADDRESS
-    );
-    const {
-      pubkeys,
-      // eslint-disable-next-line camelcase
-      withdrawal_credentials,
-      signatures,
-      // eslint-disable-next-line camelcase
-      deposit_data_roots,
-    } = data.data;
+      // setup tx variables
+      const batchDeposit = new this.web3.eth.Contract(
+        JSON.parse(BATCH_DEPOSIT_CONTRACT_ABI),
+        BATCH_DEPOSIT_CONTRACT_ADDRESS,
+      );
+      const {
+        pubkeys,
+        // eslint-disable-next-line camelcase
+        withdrawal_credentials,
+        signatures,
+        // eslint-disable-next-line camelcase
+        deposit_data_roots,
+      } = data.data;
 
-    // craft staking transaction, using the batch deposit contracts
-    return {
-      from: walletAddress,
-      to: BATCH_DEPOSIT_CONTRACT_ADDRESS,
-      data: batchDeposit.methods
-        .batchDeposit(
-          pubkeys.map((v) => '0x' + v),
-          withdrawal_credentials.map((v) => '0x' + v),
-          signatures.map((v) => '0x' + v),
-          deposit_data_roots.map((v) => '0x' + v)
-        )
-        .encodeABI(),
-      value: amount.toString(),
-      chainId: this.testnet ? '0x5' : '0x1',
-    };
+      // craft staking transaction, using the batch deposit contracts
+      return {
+        from: walletAddress,
+        to: BATCH_DEPOSIT_CONTRACT_ADDRESS,
+        data: batchDeposit.methods
+          .batchDeposit(
+            pubkeys.map((v) => '0x' + v),
+            withdrawal_credentials.map((v) => '0x' + v),
+            signatures.map((v) => '0x' + v),
+            deposit_data_roots.map((v) => '0x' + v),
+          )
+          .encodeABI(),
+        value: amount.toString(),
+        chainId: this.testnet ? '0x5' : '0x1',
+      };
+    } catch (err: any) {
+      throw new Error(err);
+    }
   }
 
   /**
@@ -92,10 +96,10 @@ export class EthService {
   async getAccountStakes(
     accountId: string,
     start: number,
-    end: number
+    end: number,
   ): Promise<EthStake> {
     const { data } = await api.get<EthStake>(
-      `/v1/eth/stakes?account=${accountId}&start=${start}?end=${end}`
+      `/v1/eth/stakes?account=${accountId}&start=${start}?end=${end}`,
     );
     return data;
   }
@@ -110,10 +114,10 @@ export class EthService {
   async getWalletStakes(
     walletAddress: string,
     start: number,
-    end: number
+    end: number,
   ): Promise<EthStake> {
     const { data } = await api.get<EthStake>(
-      `/v1/eth/stakes?wallets=${walletAddress}&start=${start}&end=${end}`
+      `/v1/eth/stakes?wallets=${walletAddress}&start=${start}&end=${end}`,
     );
     return data;
   }
@@ -125,7 +129,7 @@ export class EthService {
    */
   async getStake(validatorAddress: string): Promise<EthStake> {
     const { data } = await api.get<EthStake>(
-      `/v1/eth/stakes/${validatorAddress}`
+      `/v1/eth/stakes/${validatorAddress}`,
     );
     return data;
   }
