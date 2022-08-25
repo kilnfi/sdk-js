846ad1fe134a209cf5f98d9566478cece385e6ca
Arno Simon <arnos29@gmail.com>
2022-08-25 14:55:57 +0200

    feat/accounts

--- src/kiln.ts
+++ src/kiln.ts
@@ -3,6 +3,7 @@ import { EthService } from './services/eth';
 import { SolService } from './services/sol';
 import { Integrations } from "./types/integrations";
 import { Rpcs } from "./types/rpcs";
+import { AccountService } from "./services/accounts";
 
 type Config = {
   apiToken: string;
@@ -14,16 +15,18 @@ type Config = {
 export class Kiln {
   eth: EthService;
   sol: SolService;
+  accounts: AccountService;
 
   constructor({ testnet, apiToken, integrations, rpcs }: Config) {
     api.defaults.headers.common.Authorization = `Bearer ${apiToken}`;
     api.defaults.headers.common['Content-Type'] = 'application/json';
     api.defaults.baseURL =
       testnet === true
-        ? 'https://api.testnet.kiln.fi/'
+        ? 'https://api.devnet.kiln.fi/'
         : 'https://api.kiln.fi/';
 
     this.eth = new EthService({ testnet, integrations, rpc: rpcs?.ethereum,  });
     this.sol = new SolService({ testnet, integrations, rpc: rpcs?.solana, });
+    this.accounts = new AccountService({ testnet });
   }
 }
---
+++ src/services/accounts.ts
@@ -0,0 +1,55 @@
+import { Account, Accounts, InternalAccountsConfig } from "../types/accounts";
+import api from "../api";
+
+export class AccountService {
+  private testnet: boolean;
+
+  constructor({ testnet = false }: InternalAccountsConfig) {
+    this.testnet = testnet;
+  }
+
+  /**
+   * Get all accounts
+   */
+  async getAll(): Promise<Account[]> {
+    const { data } = await api.get<Accounts>('/v1/accounts');
+    return data.accounts;
+  }
+
+  /**
+   * Get an account by its id
+   * @param accountId
+   */
+  async get(accountId: string): Promise<Account> {
+    const { data } = await api.get<Account>(`/v1/accounts/${accountId}`);
+    return data;
+  }
+
+  /**
+   * Create an account with given name and description. The account name must be unique.
+   * @param name
+   * @param description
+   */
+  async create(name: string, description?: string): Promise<Account> {
+    const { data } = await api.post<Account>('/v1/accounts', {
+      name,
+      description: description ?? ''
+    });
+    return data;
+  }
+
+  /**
+   * Update given account
+   * @param accountId
+   * @param name
+   * @param description
+   */
+  async update(accountId: string, name: string, description?: string): Promise<Account> {
+    const { data } = await api.put<Account>(`/v1/accounts/${accountId}`, {
+      name,
+      description: description ?? ''
+    });
+    return data;
+  }
+
+}
