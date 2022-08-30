1a58e699792f59f29376a6186f22060c9573515a
Arno Simon <arnos29@gmail.com>
2022-08-30 12:28:13 +0200

    update readme

--- examples/eth.ts
+++ examples/eth.ts
@@ -21,15 +21,15 @@ const f = async () => {
   });
 
   try {
-    // const tx = await k.eth.craftStakeTx(
-    //   '376acfff-e35d-4b7c-90da-c6acb8ea7197',
-    //   '0x9cE658155A6f05FE4aef83b7Fa8F431D5e8CcB55',
-    //   32,
-    // );
-    // console.log(tx);
-    // const txSigned = await k.eth.sign('vault1', tx);
-    // const receipt = await k.eth.broadcast(txSigned);
-    // console.log(receipt);
+    const tx = await k.eth.craftStakeTx(
+      '376acfff-e35d-4b7c-90da-c6acb8ea7197',
+      '0x9cE658155A6f05FE4aef83b7Fa8F431D5e8CcB55',
+      32,
+    );
+    console.log(tx);
+    const txSigned = await k.eth.sign('vault1', tx);
+    const receipt = await k.eth.broadcast(txSigned);
+    console.log(receipt);
 
     const rewards = await k.eth.getStakesRewards(['0xb15c8670d36abe47f03acaa3ba901ac433973048d43df4d2086de4c422ea4d3d0a6b63702d253c15712d3c85997f0071', 'a262bbe9197b0fe7ced73f8af4b13813e0e03a42d62bd223e96b31c8ba379d6c56835c6729647139963f3fade95f52f6']);
     console.log(rewards);
