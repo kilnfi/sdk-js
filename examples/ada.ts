9375eb636501542ddd69a34e3f45d631510fca8e
Arno Simon <arnos29@gmail.com>
2022-09-05 10:46:01 +0200

    wip

---
+++ examples/ada.ts
@@ -0,0 +1,12 @@
+const { WalletServer } = require('cardano-wallet-js');
+import { Transaction } from '@stricahq/typhonjs';
+
+
+const f = async () => {
+  try {
+  } catch (err){
+    console.log(err);
+  }
+};
+
+f();
