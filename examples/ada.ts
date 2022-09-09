e4ad8be0ef0e309894faf71bf709f3d1d299c37b
Arno Simon <arnos29@gmail.com>
2022-09-09 18:04:22 +0200

    wip

--- examples/ada.ts
+++ examples/ada.ts
@@ -23,9 +23,9 @@ const f = async () => {
       '376acfff-e35d-4b7c-90da-c6acb8ea7197',
       'addr_test1qpy358g8glafrucevf0rjpmzx2k5esn5uvjh7dzuakpdhv4g2egyt3y3qw6jrguz0lmyhxygjdg2ytaf5z6ueaety7dsmpcee5',
     );
-    // const txSigned = await k.ada.sign('vault1', tx);
-    // const hash = await k.ada.broadcast(txSigned);
-    // console.log(hash);
+    const txSigned = await k.ada.sign('vault1', tx);
+    const hash = await k.ada.broadcast(txSigned);
+    console.log(hash);
   } catch (err){
     console.log(err);
   }
