a657eb623a061259725ce5c5bee900a5ba9d34b7
Arno Simon <arnos29@gmail.com>
2023-03-29 18:15:03 +0200

    eth-sign-and-broadcast

--- src/integrations/fb_signer.ts
+++ src/integrations/fb_signer.ts
@@ -2,10 +2,14 @@ import {
   CreateTransactionResponse,
   FireblocksSDK,
   PeerType,
+  TransactionArguments,
   TransactionOperation,
   TransactionResponse,
   TransactionStatus,
 } from 'fireblocks-sdk';
+
+import { utils } from 'ethers';
+import { EthTx } from '../types/eth';
 
 type AssetId =
   'SOL_TEST'
@@ -56,28 +60,62 @@ export class FbSigner {
 
 
   /**
-   * Sign a transaction with fireblocks
+   * Sign a transaction with fireblocks using Fireblocks raw message signing feature
    * @param payloadToSign: transaction data in hexadecimal
    * @param assetId: fireblocks asset id
-   * @param note: fireblocks custom note
+   * @param note: optional fireblocks custom note
    */
   public async signWithFB(payloadToSign: any, assetId: AssetId, note?: string): Promise<TransactionResponse> {
     try {
-      const fbTx = await this.fireblocks.createTransaction(
-        {
-          assetId: assetId,
-          operation: TransactionOperation.RAW,
-          source: {
-            type: PeerType.VAULT_ACCOUNT,
-            id: this.vaultId.toString(),
-          },
-          note,
-          extraParameters: payloadToSign,
+      const tx: TransactionArguments = {
+        assetId: assetId,
+        operation: TransactionOperation.RAW,
+        source: {
+          type: PeerType.VAULT_ACCOUNT,
+          id: this.vaultId.toString(),
         },
-      );
+        note,
+        extraParameters: payloadToSign,
+      };
+      const fbTx = await this.fireblocks.createTransaction(tx);
       return (await this.waitForTxCompletion(fbTx));
     } catch (err: any) {
       throw new Error('Fireblocks signer (signWithFB): ' + err);
+    }
+  }
+
+  /**
+   * Sign and broadcast a transaction with fireblocks using Fireblocks contract call feature
+   * @param payloadToSign: transaction data in hexadecimal
+   * @param assetId: fireblocks asset id
+   * @param note: optional fireblocks custom note
+   * @param ethTx Ethereum transaction
+   * @param destinationId Fireblocks destination id, this corresponds to the Fireblocks whitelisted contract address id
+   */
+  public async signAndBroadcastWithFB(payloadToSign: any, assetId: AssetId, ethTx: EthTx, destinationId: string, note?: string): Promise<TransactionResponse> {
+    try {
+      const tx: TransactionArguments = {
+        assetId: assetId,
+        operation: TransactionOperation.CONTRACT_CALL,
+        source: {
+          type: PeerType.VAULT_ACCOUNT,
+          id: this.vaultId.toString(),
+        },
+        destination: {
+          type: PeerType.EXTERNAL_WALLET,
+          id: destinationId,
+        },
+        amount: utils.formatEther(ethTx.data.amount_wei),
+        note,
+        extraParameters: payloadToSign,
+        gasLimit: ethTx.data.gas_limit,
+        priorityFee: utils.formatUnits(ethTx.data.max_priority_fee_per_gas_wei, 'gwei'),
+        maxFee: utils.formatUnits(ethTx.data.max_fee_per_gas_wei, 'gwei'),
+      };
+      const fbTx = await this.fireblocks.createTransaction(tx);
+      return (await this.waitForTxCompletion(fbTx));
+    } catch (err: any) {
+      throw new Error('Fireblocks signer (signAndBroadcastWithFB): ' + err);
     }
   }
 }
