ec441ea7caabbd50aa7b3cc7539decbfc2f885b6
Arno Simon <arnos29@gmail.com>
2024-09-05 15:32:19 +0200

    add POL support + deprecate MATIC

--- src/services/pol.ts
+++ src/services/pol.ts
@@ -2,90 +2,82 @@ import api from "../api";
 import { Service } from "./service";
 import { utils } from "ethers";
 import { ServiceProps } from "../types/service";
-import { MaticDecodedTx, MaticSignedTx, MaticTx, MaticTxHash, MaticTxStatus } from "../types/matic";
+import { PolDecodedTx, PolSignedTx, PolTx, PolTxHash, PolTxStatus } from "../types/pol";
 import { Integration } from "../types/integrations";
 import { TransactionResponse } from "fireblocks-sdk";
 
-export class MaticService extends Service {
+export class PolService extends Service {
   constructor({ testnet }: ServiceProps) {
     super({ testnet });
   }
 
   /**
-   * @deprecated Please use POL functions instead
-   * Craft an approve transaction to the MATIC token contract allowing the contract given to spend the amount given
+   * Craft an approve transaction to the POL token contract allowing the contract given to spend the amount given
    * If no amount is provided, an infinite amount will be approved
    * @param walletAddress wallet address signing the transaction
    * @param contractAddressToApprove contract address that you allow to spend the token
-   * @param amountMatic how many tokens to approve the spending, if not specified an infinite amount will be approved
+   * @param amountPol how many tokens to approve the spending, if not specified an infinite amount will be approved
    */
-  async craftApproveTx(
-    walletAddress: string,
-    contractAddressToApprove: string,
-    amountMatic?: number,
-  ): Promise<MaticTx> {
-    const { data } = await api.post<MaticTx>(`/v1/matic/transaction/approve`, {
+  async craftApproveTx(walletAddress: string, contractAddressToApprove: string, amountPol?: number): Promise<PolTx> {
+    const { data } = await api.post<PolTx>(`/v1/pol/transaction/approve`, {
       wallet: walletAddress,
       contract: contractAddressToApprove,
-      amount_wei: amountMatic ? this.maticToWei(amountMatic?.toString()) : undefined,
+      amount_wei: amountPol ? this.polToWei(amountPol.toString()) : undefined,
     });
     return data;
   }
 
   /**
-   * @deprecated Please use POL functions instead
    * Craft a buyVoucher transaction to a ValidatorShare proxy contract
    * It also links the stake to the account id given
    * @param accountId id of the kiln account to use for the stake transaction
    * @param walletAddress withdrawal creds /!\ losing it => losing the ability to withdraw
    * @param validatorShareProxyAddress ValidatorShare proxy contract address of the validator
-   * @param amountMatic how many tokens to stake in MATIC
+   * @param amountPol how many tokens to stake in POL
    */
   async craftBuyVoucherTx(
     accountId: string,
     walletAddress: string,
     validatorShareProxyAddress: string,
-    amountMatic: number,
-  ): Promise<MaticTx> {
-    const { data } = await api.post<MaticTx>(`/v1/matic/transaction/buy-voucher`, {
+    amountPol: number,
+  ): Promise<PolTx> {
+    const { data } = await api.post<PolTx>(`/v1/pol/transaction/buy-voucher`, {
       account_id: accountId,
       wallet: walletAddress,
-      amount_wei: this.maticToWei(amountMatic.toString()),
+      amount_wei: this.polToWei(amountPol.toString()),
       validator_share_proxy_address: validatorShareProxyAddress,
     });
     return data;
   }
 
   /**
-   * @deprecated Please use POL functions instead
    * Craft a sellVoucher transaction to a ValidatorShare proxy contract
    * Note there that your tokens will be unbonding and locked for 21 days after this transaction
    * @param walletAddress address delegating
    * @param validatorShareProxyAddress ValidatorShare proxy contract address of the validator
-   * @param amountMatic how many tokens to unbond in MATIC
+   * @param amountPol how many tokens to unbond in POL
    */
   async craftSellVoucherTx(
     walletAddress: string,
     validatorShareProxyAddress: string,
-    amountMatic: number,
-  ): Promise<MaticTx> {
-    const { data } = await api.post<MaticTx>(`/v1/matic/transaction/sell-voucher`, {
+    amountPol: number,
+  ): Promise<PolTx> {
+    const { data } = await api.post<PolTx>(`/v1/pol/transaction/sell-voucher`, {
       wallet: walletAddress,
-      amount_wei: this.maticToWei(amountMatic.toString()),
+      amount_wei: this.polToWei(amountPol.toString()),
       validator_share_proxy_address: validatorShareProxyAddress,
     });
     return data;
   }
 
   /**
-   * @deprecated Please use POL functions instead
    * Craft an unstakeClaimTokens transaction to a ValidatorShare proxy contract
    * Note that your tokens must be unbonded before you can claim them
    * @param walletAddress address delegating
    * @param validatorShareProxyAddress ValidatorShare proxy contract address of the validator
    */
-  async craftUnstakeClaimTokensTx(walletAddress: string, validatorShareProxyAddress: string): Promise<MaticTx> {
-    const { data } = await api.post<MaticTx>(`/v1/matic/transaction/unstake-claim-tokens`, {
+  async craftUnstakeClaimTokensTx(walletAddress: string, validatorShareProxyAddress: string): Promise<PolTx> {
+    const { data } = await api.post<PolTx>(`/v1/pol/transaction/unstake-claim-tokens`, {
       wallet: walletAddress,
       validator_share_proxy_address: validatorShareProxyAddress,
     });
@@ -93,14 +85,13 @@ export class MaticService extends Service {
   }
 
   /**
-   * @deprecated Please use POL functions instead
    * Craft an withdrawRewards transaction to a ValidatorShare proxy contract
    * All rewards earned are transferred to the delegator's wallet
    * @param walletAddress address delegating
    * @param validatorShareProxyAddress ValidatorShare proxy contract address of the validator
    */
-  async craftWithdrawRewardsTx(walletAddress: string, validatorShareProxyAddress: string): Promise<MaticTx> {
-    const { data } = await api.post<MaticTx>(`/v1/matic/transaction/withdraw-rewards`, {
+  async craftWithdrawRewardsTx(walletAddress: string, validatorShareProxyAddress: string): Promise<PolTx> {
+    const { data } = await api.post<PolTx>(`/v1/pol/transaction/withdraw-rewards`, {
       wallet: walletAddress,
       validator_share_proxy_address: validatorShareProxyAddress,
     });
@@ -108,14 +99,13 @@ export class MaticService extends Service {
   }
 
   /**
-   * @deprecated Please use POL functions instead
    * Craft an withdrawRewards transaction to a ValidatorShare proxy contract
    * All rewards earned are then re-delegated
    * @param walletAddress address delegating
    * @param validatorShareProxyAddress ValidatorShare proxy contract address of the validator
    */
-  async craftRestakeRewardsTx(walletAddress: string, validatorShareProxyAddress: string): Promise<MaticTx> {
-    const { data } = await api.post<MaticTx>(`/v1/matic/transaction/restake-rewards`, {
+  async craftRestakeRewardsTx(walletAddress: string, validatorShareProxyAddress: string): Promise<PolTx> {
+    const { data } = await api.post<PolTx>(`/v1/pol/transaction/restake-rewards`, {
       wallet: walletAddress,
       validator_share_proxy_address: validatorShareProxyAddress,
     });
@@ -123,13 +113,12 @@ export class MaticService extends Service {
   }
 
   /**
-   * @deprecated Please use POL functions instead
    * Sign transaction with given integration
    * @param integration custody solution to sign with
    * @param tx raw transaction
    * @param note note to identify the transaction in your custody solution
    */
-  async sign(integration: Integration, tx: MaticTx, note?: string): Promise<MaticSignedTx> {
+  async sign(integration: Integration, tx: PolTx, note?: string): Promise<PolSignedTx> {
     const payload = {
       rawMessageData: {
         messages: [
@@ -145,9 +134,9 @@ export class MaticService extends Service {
     };
 
     const fbSigner = this.getFbSigner(integration);
-    const fbNote = note ? note : "MATIC tx from @kilnfi/sdk";
+    const fbNote = note ? note : "POL tx from @kilnfi/sdk";
     const fbTx = await fbSigner.sign(payload, this.testnet ? "ETH_TEST5" : "ETH", fbNote);
-    const { data } = await api.post<MaticSignedTx>(`/v1/matic/transaction/prepare`, {
+    const { data } = await api.post<PolSignedTx>(`/v1/pol/transaction/prepare`, {
       unsigned_tx_serialized: tx.data.unsigned_tx_serialized,
       r: `0x${fbTx?.signedMessages?.[0].signature.r}`,
       s: `0x${fbTx?.signedMessages?.[0].signature.s}`,
@@ -158,13 +147,12 @@ export class MaticService extends Service {
   }
 
   /**
-   * @deprecated Please use POL functions instead
    * Sign transaction with given integration
    * @param integration custody solution to sign with
    * @param tx raw transaction
    * @param note note to identify the transaction in your custody solution
    */
-  async signAndBroadcast(integration: Integration, tx: MaticTx, note?: string): Promise<TransactionResponse> {
+  async signAndBroadcast(integration: Integration, tx: PolTx, note?: string): Promise<TransactionResponse> {
     if (!integration.fireblocksDestinationId) {
       throw new Error("Fireblocks destination id is missing in integration");
     }
@@ -174,7 +162,7 @@ export class MaticService extends Service {
     };
 
     const fbSigner = this.getFbSigner(integration);
-    const fbNote = note ? note : "MATIC tx from @kilnfi/sdk";
+    const fbNote = note ? note : "POL tx from @kilnfi/sdk";
     const assetId = this.testnet ? "ETH_TEST5" : "ETH";
     return await fbSigner.signAndBroadcastWith(
       payload,
@@ -187,43 +175,39 @@ export class MaticService extends Service {
   }
 
   /**
-   * @deprecated Please use POL functions instead
    * Broadcast transaction to the network
    * @param signedTx
    */
-  async broadcast(signedTx: MaticSignedTx): Promise<MaticTxHash> {
-    const { data } = await api.post<MaticTxHash>(`/v1/matic/transaction/broadcast`, {
+  async broadcast(signedTx: PolSignedTx): Promise<PolTxHash> {
+    const { data } = await api.post<PolTxHash>(`/v1/pol/transaction/broadcast`, {
       tx_serialized: signedTx.data.signed_tx_serialized,
     });
     return data;
   }
 
   /**
-   * @deprecated Please use POL functions instead
    * Get transaction status
    * @param txHash transaction hash
    */
-  async getTxStatus(txHash: string): Promise<MaticTxStatus> {
-    const { data } = await api.get<MaticTxStatus>(`/v1/matic/transaction/status?tx_hash=${txHash}`);
+  async getTxStatus(txHash: string): Promise<PolTxStatus> {
+    const { data } = await api.get<PolTxStatus>(`/v1/pol/transaction/status?tx_hash=${txHash}`);
     return data;
   }
 
   /**
-   * @deprecated Please use POL functions instead
    * Decode transaction
    * @param txSerialized transaction serialized
    */
-  async decodeTx(txSerialized: string): Promise<MaticDecodedTx> {
-    const { data } = await api.get<MaticDecodedTx>(`/v1/matic/transaction/decode?tx_serialized=${txSerialized}`);
+  async decodeTx(txSerialized: string): Promise<PolDecodedTx> {
+    const { data } = await api.get<PolDecodedTx>(`/v1/pol/transaction/decode?tx_serialized=${txSerialized}`);
     return data;
   }
 
   /**
-   * @deprecated Please use POL functions instead
-   * Convert MATIC to WEI
-   * @param matic
+   * Convert POL to WEI
+   * @param pol
    */
-  maticToWei(matic: string): string {
-    return utils.parseEther(matic).toString();
+  polToWei(pol: string): string {
+    return utils.parseEther(pol).toString();
   }
 }
