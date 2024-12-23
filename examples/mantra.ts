3f05019ced83790724ee9feff1e193288a10aad1
ilies <ilieszareb2@gmail.com>
2024-12-23 17:01:16 +0100

    remove ts ignore example mantra

--- examples/mantra.ts
+++ examples/mantra.ts
@@ -19,25 +19,22 @@ const vault: FireblocksIntegration = {
 
 try {
   console.log('crafting...');
-  //@ts-ignore
   const tx = await k.client.POST('/mantra/transaction/stake', {
     body: {
       account_id: process.env.KILN_ACCOUNT_ID as string,
       pubkey: '028dfa6f41c655e38a0f8f2e3f3aa3e1246907a9bb299933f11996e2a345a21e10',
       validator: 'mantravaloper146mj09yzu3mvz7pmy4dvs4z9wr2mst7ram37xw',
       amount_uom: omToUom('0.01').toString(),
+      restake_rewards: false,
     },
   });
 
   console.log(tx);
   console.log('signing...');
-  //@ts-ignore
   if (!tx.data?.data) throw new Error('No data in response');
-  //@ts-ignore
   const signResponse = await k.fireblocks.signMantraTx(vault, tx.data.data);
   console.log('broadcasting...');
   if (!signResponse.signed_tx?.data?.signed_tx_serialized) throw new Error('No signed_tx in response');
-  //@ts-ignore
   const broadcastedTx = await k.client.POST('/mantra/transaction/broadcast', {
     body: {
       tx_serialized: signResponse.signed_tx.data.signed_tx_serialized,
