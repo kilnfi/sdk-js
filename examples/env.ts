bb113f74effc1baed8a97c40e78cfb30e4e1735e
Martin Saldinger <martin.saldinger@gmail.com>
2025-01-08 12:15:12 +0100

    cleanup example

---
+++ examples/env.ts
@@ -0,0 +1,37 @@
+export const loadEnv = async () => {
+  if (!Bun.env.KILN_API_URL) {
+    console.log('KILN_API_URL is required');
+    process.exit(1);
+  }
+
+  if (!Bun.env.KILN_ACCOUNT_ID) {
+    console.log('KILN_ACCOUNT_ID is required');
+    process.exit(1);
+  }
+
+  if (!Bun.env.KILN_API_KEY) {
+    console.log('KILN_API_KEY is required');
+    process.exit(1);
+  }
+
+  if (!Bun.env.FIREBLOCKS_API_KEY) {
+    console.log('FIREBLOCKS_API_KEY is required');
+    process.exit(1);
+  }
+
+  if (!Bun.env.FIREBLOCKS_VAULT_ID) {
+    console.log('FIREBLOCKS_VAULT_ID is required');
+    process.exit(1);
+  }
+
+  const fireblocksApiSecret = await Bun.file('fireblocks_secret_prod.key').text();
+
+  return {
+    kilnApiUrl: Bun.env.KILN_API_URL,
+    kilnApiKey: Bun.env.KILN_API_KEY,
+    kilnAccountId: Bun.env.KILN_ACCOUNT_ID,
+    fireblocksApiKey: Bun.env.FIREBLOCKS_API_KEY,
+    fireblocksVaultId: Bun.env.FIREBLOCKS_VAULT_ID as `${number}`,
+    fireblocksApiSecret,
+  };
+};
--- examples/kava.ts
+++ examples/kava.ts
@@ -1,48 +1,86 @@
-import { kavaToUkava, Kiln } from "../src/kiln";
-import fs from "node:fs";
-import 'dotenv/config'
-import type { FireblocksIntegration } from "../src/fireblocks.ts";
+import { kavaToUkava, Kiln, KILN_VALIDATORS } from '../src/kiln.ts';
+import type { FireblocksIntegration } from '../src/fireblocks.ts';
+import { loadEnv } from './env.ts';
 
-
-const apiSecret = fs.readFileSync(`${__dirname}/fireblocks_secret_prod.key`, 'utf8');
+const { kilnApiKey, kilnAccountId, kilnApiUrl, fireblocksApiKey, fireblocksApiSecret, fireblocksVaultId } =
+  await loadEnv();
 
 const k = new Kiln({
-  baseUrl: process.env.KILN_API_URL as string,
-  apiToken: process.env.KILN_API_KEY as string,
+  baseUrl: kilnApiUrl,
+  apiToken: kilnApiKey,
 });
 
 const vault: FireblocksIntegration = {
-  provider: 'fireblocks',
-  fireblocksApiKey: process.env.FIREBLOCKS_API_KEY as string,
-  fireblocksSecretKey: apiSecret,
-  vaultId: 37
+  config: {
+    apiKey: fireblocksApiKey,
+    secretKey: fireblocksApiSecret,
+    basePath: 'https://api.fireblocks.io/v1',
+  },
+  vaultId: fireblocksVaultId,
 };
 
-try {
-  console.log('crafting...');
-  const tx = await k.client.POST(
-    '/kava/transaction/stake',
-    {
-      body: {
-        account_id: process.env.KILN_ACCOUNT_ID as string,
-        pubkey: '0233335b6c68a85e01b85055d0e8c2fcef42fed977898422ef3a5f6baf9a9a413e',
-        validator: 'kavavaloper1djqecw6nn5tydxq0shan7srv8j65clsf79myt8',
-        amount_ukava: kavaToUkava('0.01').toString(),
-      }
-    }
-  );
-  console.log('signing...');
-  if(!tx.data?.data) throw new Error('No data in response');
-  const signResponse = await k.fireblocks.signKavaTx(vault, tx.data.data);
-  console.log('broadcasting...');
-  if(!signResponse.signed_tx?.data?.signed_tx_serialized) throw new Error('No signed_tx in response');
-  const broadcastedTx = await k.client.POST("/kava/transaction/broadcast", {
-    body: {
-      tx_serialized: signResponse.signed_tx.data.signed_tx_serialized,
-    }
-  });
-  console.log(broadcastedTx);
+//
+// Get the pubkey from Fireblocks
+//
+const fireblocksPubkey = (
+  await k.fireblocks.getSdk(vault).vaults.getPublicKeyInfo({
+    algorithm: 'MPC_ECDSA_SECP256K1',
+    derivationPath: JSON.stringify([44, 459, Number(vault.vaultId), 0, 0]),
+    compressed: true,
+  })
+).data.publicKey;
+if (!fireblocksPubkey) {
+  console.log('Failed to get pubkey');
+  process.exit(0);
+}
 
-} catch (err) {
-  console.log(err);
+//
+// Craft the transaction
+//
+console.log('Crafting transaction...');
+const txRequest = await k.client.POST('/kava/transaction/stake', {
+  body: {
+    account_id: kilnAccountId,
+    pubkey: fireblocksPubkey,
+    validator: KILN_VALIDATORS.KAVA.mainnet.KILN,
+    amount_ukava: kavaToUkava('0.01').toString(),
+  },
+});
+if (txRequest.error) {
+  console.log('Failed to craft transaction:', txRequest);
+  process.exit(1);
+} else {
+  console.log('Crafted transaction:', txRequest.data);
+}
+console.log('\n\n\n');
+
+//
+// Sign the transaction
+//
+console.log('Signing transaction...');
+const signRequest = await (async () => {
+  try {
+    return await k.fireblocks.signKavaTx(vault, txRequest.data.data);
+  } catch (err) {
+    console.log('Failed to sign transaction:', err);
+    process.exit(1);
+  }
+})();
+console.log('Signed transaction:', signRequest);
+console.log('\n\n\n');
+
+//
+// Broadcast the transaction
+//
+console.log('Broadcasting transaction...');
+const broadcastedRequest = await k.client.POST('/kava/transaction/broadcast', {
+  body: {
+    tx_serialized: signRequest.signed_tx.data.signed_tx_serialized,
+  },
+});
+if (broadcastedRequest.error) {
+  console.log('Failed to broadcast transaction:', broadcastedRequest);
+  process.exit(1);
+} else {
+  console.log('Broadcasted transaction:', broadcastedRequest.data);
 }
